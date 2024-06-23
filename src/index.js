a93778c11dfced9853b923e76bf46fa05664952c
GITzhouhub <zhoutao0108@163.com>
2024-06-23 19:11:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.faskworks.xyz": dockerHub,
+  "quay.faskworks.xyz": "https://quay.io",
+  "gcr.faskworks.xyz": "https://gcr.io",
+  "k8s-gcr.faskworks.xyz": "https://k8s.gcr.io",
+  "k8s.faskworks.xyz": "https://registry.k8s.io",
+  "ghcr.faskworks.xyz": "https://ghcr.io",
+  "cloudsmith.faskworks.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.faskworks.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
